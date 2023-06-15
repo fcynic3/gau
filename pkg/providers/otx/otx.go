d0a45ebe8276907ddf6622c9fb35b43db9800c1b
fcynic3 <117019191+fcynic3@users.noreply.github.com>
2023-06-15 17:27:57 +0545

    Update otx.go

--- pkg/providers/otx/otx.go
+++ pkg/providers/otx/otx.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"github.com/bobesa/go-domain-util/domainutil"
 	jsoniter "github.com/json-iterator/go"
-	"github.com/lc/gau/v2/pkg/httpclient"
-	"github.com/lc/gau/v2/pkg/providers"
+	"github.com/fcynic3/gau/v2/pkg/httpclient"
+	"github.com/fcynic3/gau/v2/pkg/providers"
 	"github.com/sirupsen/logrus"
 )
 
